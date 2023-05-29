5bd68526872967368326d639f7084509deda22ed
belutreachi <belutreachi22@gmail.com>
2023-05-29 14:59:42 -0300

    se agrego funcion para calcular

--- funciones.js
+++ funciones.js
@@ -24,3 +24,21 @@ let sumar = () => {
     document.getElementById("totalS").innerHTML;
     document.operacionesMat.sum_total.value = re;
 }
+
+let calcular = () => {
+    let vi, di, a, tabla, res, t, vf, df, fila, celdavf, celdadf;
+    vi = Number(document.getElementById("v0").value);
+    di = Number(document.getElementById("x0").value);
+    a = Number(document.getElementById("a").value);
+
+    res = document.getElementById("resultados");
+
+    for (t=0; t<10; t++){
+        df = di + (vi * t) + (0.5 * a * Math.pow(t, 2));
+        vf = vi + (a*t);
+
+        document.getElementById("d_total").innerHTML = df.toFixed(2);
+        document.getElementById("v_total").innerHTML = vf.toFixed(2);
+    }
+
+}
